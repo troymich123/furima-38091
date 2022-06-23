90990e4fff61d30da984b508c0fffb1201ca4dcd
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-23 21:24:40 +0900

    ユーザー登録バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
 
-  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英字と数字を含めて設定してください' } do
-    validates :password
-  end
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :first_name
     validates :last_name
   end  
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: '全角（カタカナ）を使用してください' } do
     validates :first_name_jp
     validates :last_name_jp
   end  
