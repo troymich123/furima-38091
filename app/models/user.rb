82abe8ddecad172590744bb0f2a86a0646bb5e6b
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-23 17:32:58 +0900

    CSSファイルの配備、モデルでのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英字と数字を含めて設定してください' } do
+    validates :encrypted_password
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end  
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角（カタカナ）を使用してください' } do
+    validates :first_name_jp
+    validates :last_name_jp
+  end  
+
+  validates :birthday, presence: true
+
 end
