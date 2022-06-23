f697fcc5d003231e06002c4e7690118f449eba4f
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-24 07:18:42 +0900

    Faker設定、Rspec初期記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
-    it '' do
+    context '新規登録できるとき' do
+      it 'nickname、email、password、password_confirmation、お名前(姓、名)、お名前カナ(姓、名)、生年月日が存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it '' do
+      end
     end
   end
 end
