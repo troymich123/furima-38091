f35217f1537de5368867b3fc5294345b039f58cf
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-24 21:03:35 +0900

    ActiveHash修正、出品DB登録完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,17 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
   private
 
-  def items_params
-    params.require(:items).permit(:image, :product_name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_until_delivery_id, :price).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_until_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
