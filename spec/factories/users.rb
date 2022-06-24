97a796e4a3dfa245f592917de56543a8512aa556
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-24 09:09:48 +0900

    Rubocop修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name {Gimei.family}
-    first_name {Gimei.given}
-    last_name_jp {Gimei.family.katakana}
-    first_name_jp {Gimei.given.katakana}
-    birthday {Faker::Date.birthday(min_age:18, max_age:65)}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name { Gimei.family }
+    first_name { Gimei.given }
+    last_name_jp { Gimei.family.katakana }
+    first_name_jp { Gimei.given.katakana }
+    birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
