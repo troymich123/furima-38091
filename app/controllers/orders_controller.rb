43cff8f10756ad811aa2472aa8bccf344effc5f9
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-29 12:37:24 +0900

    FormオブジェクトからDB保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,15 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_shipment = OrderShipment.new
   end
 
   def create
-    @order = Item.find(params[:item_id])
-    if @order.valid?
-      @order.save
-      return redirect_to root_path    
+    @item = Item.find(params[:item_id])
+    @order_shipment = OrderShipment.new(order_params)
+    if @order_shipment.valid?
+      @order_shipment.save
+      return redirect_to root_path
     else
       render 'index'
     end
@@ -17,7 +19,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:price)
+    params.require(:order_shipment).permit(:postcode, :prefecture, :city, :street, :address_other, :phone_num).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
