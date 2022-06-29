33f7e7389747ff4452eff45619798529e3990894
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-06-29 12:42:24 +0900

    住所未入力エラー表示

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -3,7 +3,7 @@ class OrderShipment
   attr_accessor :postcode, :prefecture, :city, :street, :address_other, :phone_num, :user_id, :item_id
 
   with_options presence: true do
-    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true, message: "is invalid. Include hyphen(-)"}
     validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
     validates :city, :street, :phone_num, :user_id, :item_id
   end
