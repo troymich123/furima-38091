1fcd9b01a4ad770434fe2828ab3ba44021a5c08c
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-01 17:22:39 +0900

    テストコード文言修正

--- spec/models/order_shipment_spec.rb
+++ spec/models/order_shipment_spec.rb
@@ -14,74 +14,74 @@ RSpec.describe OrderShipment, type: :model do
         expect(@order_shipment).to be_valid
       end
 
-      it "address_otherは空でも保存できること" do
+      it "address_otherは空でも購入できること" do
         @order_shipment.address_other = ''
         expect(@order_shipment).to be_valid
       end
     end
 
     context '内容に問題がある場合' do
-      it "postcodeが空では保存できないこと" do
+      it "postcodeが空では購入できないこと" do
         @order_shipment.postcode = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Postcode can't be blank")
       end
 
-      it "postcodeがハイフン無しでは保存できないこと" do
+      it "postcodeがハイフン無しでは購入できないこと" do
         @order_shipment.postcode = Faker::Number.number(digits: 7)
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Postcode is invalid. Include hyphen(-)")
       end
 
-      it "prefectureが空では保存できないこと" do
+      it "prefectureが空では購入できないこと" do
         @order_shipment.prefecture_id = 1
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Prefecture can't be blank")
       end
 
-      it "cityが空では保存できないこと" do
+      it "cityが空では購入できないこと" do
         @order_shipment.city = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("City can't be blank")
       end
 
-      it "streetが空では保存できないこと" do
+      it "streetが空では購入できないこと" do
         @order_shipment.street = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Street can't be blank")
       end
 
-      it "phone_numが空では保存できないこと" do
+      it "phone_numが空では購入できないこと" do
         @order_shipment.phone_num = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Phone num can't be blank")
       end
 
-      it "phone_numが9桁以下の半角数値では保存できないこと" do
+      it "phone_numが9桁以下の半角数値では購入できないこと" do
         @order_shipment.phone_num = Faker::Number.number(digits: 9)
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Phone num is invalid. Must be 10 or 11 digits")
       end
 
-      it "phone_numが12桁以上の半角数値では保存できないこと" do
+      it "phone_numが12桁以上の半角数値では購入できないこと" do
         @order_shipment.phone_num = Faker::Number.number(digits: 12)
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Phone num is invalid. Must be 10 or 11 digits")
       end
 
-      it "user_idが空では保存できないこと" do
+      it "user_idが空では購入できないこと" do
         @order_shipment.user_id = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("User can't be blank")
       end
 
-      it "item_idが空では保存できないこと" do
+      it "item_idが空では購入できないこと" do
         @order_shipment.item_id = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Item can't be blank")
       end
 
-      it 'tokenが空では登録できないこと' do
+      it 'tokenが空では購入できないこと' do
         @order_shipment.token = ''
         @order_shipment.valid?
         expect(@order_shipment.errors.full_messages).to include("Token can't be blank")
